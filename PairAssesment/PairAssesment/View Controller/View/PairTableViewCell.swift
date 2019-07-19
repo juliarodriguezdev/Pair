db427587f51ec46a21597a8e715c928fbe9559c7
Julia Rodriguez <juliarodriguez@Julias-MacBook-Pro.local>
2019-07-19 09:51:05 -0600

    adds model, model controller, view, view controller, and sets up storyboard scene

---
+++ PairAssesment/PairAssesment/View Controller/View/PairTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PairTableViewCell.swift
+//  PairAssesment
+//
+//  Created by Julia Rodriguez on 7/19/19.
+//  Copyright © 2019 Julia Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PairTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
